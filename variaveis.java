dc1eb72bdd8a3c5b204dfaab3048182e002e16ca
Lucas Salvador Amorim <lucassalvadoramorim@gmail.com>
2022-06-06 17:03:47 +0000

    variaveis p2

---
+++ variaveis.java
@@ -0,0 +1,14 @@
+public class variaveis{
+    public static void main(String[] args) {
+       
+   int segundaSemana = 5;
+   int terceiraSemana = 2;
+   int quartaSemana = 7;
+
+    System.out.println(segundaSemana);
+    System.out.println(terceiraSemana);
+    System.out.println(quartaSemana);
+
+    }
+   
+}
